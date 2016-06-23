01ebb5cd4a9c4ca49650e89d42ecc76ca5dacfe7
moisesramirez <moises.ramon.ramirez@gmail.com>
2016-06-23 19:48:32 -0400

    Drawing sketch.

--- js/script.js
+++ js/script.js
@@ -1,18 +1,68 @@
+
+var objects = [];
+objects.push({
+	x:0, y:50,
+	width:50, heigth:250,
+	color: '#00f'
+});
+
+objects.push({
+	x:100, y:100,
+	width:50, heigth:250,
+	color: '#0f0'
+});
+
+objects.push({
+	x:200, y:100,
+	width:50, heigth:250,
+	color: '#0f0'
+});
+
+objects.push({
+	x:300, y:100,
+	width:50, heigth:250,
+	color: '#0f0'
+});
+
+objects.push({
+	x:400, y:100,
+	width:50, heigth:250,
+	color: '#0f0'
+});
+
+objects.push({
+	x:500, y:100,
+	width:50, heigth:250,
+	color: '#0f0'
+});
+
+objects.push({
+	x:600, y:50,
+	width:50, heigth:250,
+	color: '#00f'
+});
+
+objects.push({
+	x:0, y:0,
+	width:700, heigth:50,
+	color: '#00f'
+});
+
+
 window.onload = function() {
 	var cv = document.getElementById("canv");
 	var cvx = cv.getContext("2d");
 
+	/*
 	cvx.fillStyle = "#FF0000";
 	cvx.moveTo(250,250);
 	cvx.lineTo(270,270);
-	
-	cvx.stroke();
+
+	cvx.stroke();*/
+	for (var i = 0; i< objects.length; i++) {
+		cvx.fillStyle = objects[i].color;
+		cvx.fillRect(objects[i].x, objects[i].y, objects[i].width, objects[i].heigth);
+
+	}
 };
 
-
-var objects = [];
-objects.push({
-	x:0, y:0,
-	width:100, heigth:200,
-	color: '#0f0'
-});
