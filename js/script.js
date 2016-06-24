0e7cac7bcb0ed68efe345acf740b9dfb0c262f8a
moisesramirez <moises.ramon.ramirez@gmail.com>
2016-06-23 21:59:55 -0400

    Sketch, and some things...

--- js/script.js
+++ js/script.js
@@ -1,52 +1,19 @@
 
 var objects = [];
-objects.push({
-	x:0, y:50,
-	width:50, heigth:250,
-	color: '#00f'
-});
+var pH = 500;//1+0.10; // original:500 - actual:550
+var pW = 650;// 1+0.3846; // original:650 - actual:900
 
-objects.push({
-	x:100, y:100,
-	width:50, heigth:250,
-	color: '#0f0'
-});
-
-objects.push({
-	x:200, y:100,
-	width:50, heigth:250,
-	color: '#0f0'
-});
-
-objects.push({
-	x:300, y:100,
-	width:50, heigth:250,
-	color: '#0f0'
-});
-
-objects.push({
-	x:400, y:100,
-	width:50, heigth:250,
-	color: '#0f0'
-});
-
-objects.push({
-	x:500, y:100,
-	width:50, heigth:250,
-	color: '#0f0'
-});
-
-objects.push({
-	x:600, y:50,
-	width:50, heigth:250,
-	color: '#00f'
-});
-
-objects.push({
-	x:0, y:0,
-	width:700, heigth:50,
-	color: '#00f'
-});
+objects.push({x:0, y:50, width:25, heigth:200, color:'#00f'});
+objects.push({x:100, y:100,	width:25, heigth:200, color:'#0f0'});
+objects.push({x:175, y:100, width:25, heigth:200, color:'#0f0'});
+objects.push({x:250, y:100, width:25, heigth:200, color:'#0f0'});
+objects.push({x:325, y:100, width:25, heigth:200, color:'#0f0'});
+objects.push({x:400, y:100,	width:25, heigth:200, color:'#0f0'});
+objects.push({x:475, y:100,	width:25, heigth:200, color:'#0f0'});
+objects.push({x:550, y:100, width:25, heigth:75, color:'#0f0'});
+objects.push({x:550, y:225, width:25, heigth:75, color:'#0f0'});
+objects.push({x:625, y:50, width:25, heigth:200, color:'#00f'});
+objects.push({x:0, y:0, width:650, heigth:50, color:'#00f'});
 
 
 window.onload = function() {
@@ -59,9 +26,16 @@ window.onload = function() {
 	cvx.lineTo(270,270);
 
 	cvx.stroke();*/
+
+	
+	pH = cv.height / pH; //esto ajusta los elementos al tamaño actual de canvas.
+	pW = cv.width / pW; // esto ajusta los elementos al tamaño actual el canvas.
+	
 	for (var i = 0; i< objects.length; i++) {
+		//console.log(cv.width);
+
 		cvx.fillStyle = objects[i].color;
-		cvx.fillRect(objects[i].x, objects[i].y, objects[i].width, objects[i].heigth);
+		cvx.fillRect(objects[i].x * pW, objects[i].y * pH, objects[i].width * pW, objects[i].heigth * pH);
 
 	}
 };
