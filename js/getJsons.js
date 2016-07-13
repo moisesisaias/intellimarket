9c28d5645a774f0f7cddd5a8855b73ae27e81b64
Junior Cesar Ventura Encarnacion <juniorventura77@gmail.com>
2016-07-13 09:46:19 -0400

    Setting up some files and structure for the proyect

---
+++ js/getJsons.js
@@ -0,0 +1,48 @@
+
+// THIS CODE DOES A GET REQUEST TO READ THE PRODUCTOS FORM A JSON FILE
+var map;
+var products;
+
+		$(document).ready(function(){
+			var jqxhr = $.getJSON( "./data/products.json", function(){
+				console.log( "Request initialized" );
+			})
+			.done(function(data) {
+				products = data.products;
+			})
+			.fail(function() {
+				console.log( "error in the request");
+			});
+
+jqxhr.complete(function() {
+			
+		$.each(products, function(i, option) {
+					$('#sel').append($('<option/>').attr("value",option.id).text(option.title));
+				});
+		//console.log("VEAMOS "+map.get('block1'));
+
+
+	});
+});
+
+
+	$(document).ready(function(){
+			var jqxhr = $.getJSON("./data/cadenaMap.json", function(){
+				console.log( "Request initialized" );
+			})
+			.done(function(data) {
+				map = data;
+			})
+			.fail(function() {
+				console.log( "error in the request");
+			});
+
+jqxhr.complete(function() {
+		console.log("Cadena Map created sucessfully");
+	});
+});
+
+	
+
+
+// THIS CODE DOES A GET REQUEST TO READ THE PRODUCTOS FORM A JSON FILE
