fe60dd28e889fd8c41349239e0d92e8f1e5e6ca0
moisesramirez <moises.ramon.ramirez@gmail.com>
2016-07-13 23:20:02 -0400

    Some issues were fixed.

--- js/nodes.js
+++ js/nodes.js
@@ -19,71 +19,71 @@ var status = "off";
 */
 
 // Primer columna
-nodes.push({"id":"fc1", x:65, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:65, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:65, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:65, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:65, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c1", "x":65, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c1", "x":65, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c1", "x":65, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c1", "x":65, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c1", "x":65, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Segunda columna
-nodes.push({"id":"fc1", x:150, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:150, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:150, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:150, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc1", x:150, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c2", "x":150, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c2", "x":150, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c2", "x":150, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c2", "x":150, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c2", "x":150, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Tercera columna
-nodes.push({"id":"fc2", x:225, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc2", x:225, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc2", x:225, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc2", x:225, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc2", x:225, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c3", "x":225, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c3", "x":225, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c3", "x":225, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c3", "x":225, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c3", "x":225, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Cuarta columna
-nodes.push({"id":"fc3", x:300, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc3", x:300, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc3", x:300, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc3", x:300, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({"id":"fc3", x:300, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c4", "x":300, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c4", "x":300, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c4", "x":300, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c4", "x":300, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c4", "x":300, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Quinta columna
-nodes.push({x:375, y:60, r:nRadio, sA:sAngle, eA:eAngle,  color:nColor, status:status});
-nodes.push({x:375, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:375, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:375, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:375, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c5", "x":375, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle,  "color":nColor, "status":status});
+nodes.push({"id":"f2c5", "x":375, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c5", "x":375, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c5", "x":375, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c5", "x":375, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Sexta columna
-nodes.push({x:450, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:450, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:450, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:450, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:450, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c6", "x":450, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c6", "x":450, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c6", "x":450, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c6", "x":450, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c6", "x":450, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Séptima columna
-nodes.push({x:525, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:525, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:525, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:525, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:525, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c7", "x":525, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c7", "x":525, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c7", "x":525, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c7", "x":525, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c7", "x":525, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 // Octava columna
-nodes.push({x:600, y:60, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:600, y:100, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:600, y:170, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:600, y:230, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
-nodes.push({x:600, y:300, r:nRadio, sA:sAngle, eA:eAngle, color:nColor, status:status});
+nodes.push({"id":"f1c8", "x":600, "y":60, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f2c8", "x":600, "y":100, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f3c8", "x":600, "y":170, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f4c8", "x":600, "y":230, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
+nodes.push({"id":"f5c8", "x":600, "y":300, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":nColor, "status":status});
 
 
 // Linea de cajeros
-nodes.push({x:65, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:150, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:225, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:300, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:375, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:450, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-nodes.push({x:525, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0', status:status});
-//nodes.push({x:600, y:375, r:nRadio, sA:sAngle, eA:eAngle, color:'0f0'}); aQUÍ SE INICIA, TAL VEZ MÁS ATRAS
+nodes.push({"x":65, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":150, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":225, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":300, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":375, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":450, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+nodes.push({"x":525, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0', "status":status});
+//nodes.push({"x":600, "y":375, "r":nRadio, "sA":sAngle, "eA":eAngle, "color":'0f0'}); aQUÍ SE INICIA, TAL VEZ MÁS ATRAS
 
 /*
 */
@@ -142,5 +142,4 @@ function drawNode(i, pH, pW) {
 	cvx.stroke();
 
 	//console.log(myNode.r);
-	}
 };
